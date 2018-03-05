04c2a639888cdf40d34f0781e3f6187ab877383b
Jovan Khoo <jovankhooyongwen@gmail.com>
2018-03-05 11:15:41 +0000

    modified edit, new, index and partial messages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -17,6 +17,13 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
   
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    flash[:notice] = "Article has been successfully deleted"
+    redirect_to articles_path
+  end
+  
   def edit
     @article = Article.find(params[:id])
   end
